9b58c8a9db365dee5170acec64761f9fad49bb65
Ari Woods <awoods226@gmail.com>
2018-07-24 20:35:12 -0500

    removing api key

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import config from "../../../config";
 const Location = () => {
   return (
     <iframe
@@ -6,7 +7,9 @@ const Location = () => {
       height="450"
       frameBorder="0"
       //   style="border:0"
-      src="https://www.google.com/maps/embed/v1/place?q=2615%20N%20Memorial%20Pkwy%20ste%20c%2C%20Huntsville%2C%20AL%2035810&key=AIzaSyC08ykGKLAKsGCdkwQmZqU_yBuvLXE1xuE"
+      src={`https://www.google.com/maps/embed/v1/place?q=2615%20N%20Memorial%20Pkwy%20ste%20c%2C%20Huntsville%2C%20AL%2035810&key=${
+        config.googleMapsAPIKey
+      }`}
       allowFullScreen
     />
   );
